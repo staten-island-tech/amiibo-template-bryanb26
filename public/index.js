4b7525b247dad4932f14dc385f1d66e36718dc72
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-12-18 11:44:48 -0500

    swag

--- public/index.js
+++ public/index.js
@@ -1,16 +1,31 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
 //define paths for express
 const publicDiretoryPath = path.join(__dirname,"../pubic");
+const partialsPath = path.join(__dirname, "../templates/partials")
 const viewsPath = path.join(__dirname, "../templates/views");
 //setup handlebars engine and views location
-app.set("views engine", "hbs");//Telling express/NODE to use handlebars for templates
+app.set('view engine', "hbs");//Telling express/NODE to use handlebars for templates
 app.set("views", viewsPath);//Telling express to get templates from templates/views folder 
-app.get("", async (req,res)=>{
+hbs.registerPartials(partialsPath);
+app.get("/about/:id", async (req,res)=>{
+    const name = req.params.id;
     try{
-        res.render("index");
+        res.render("index", {
+            title: name
+        });
+    }catch(error){
+        res.status(500).send("Page not found");
+    }
+});
+
+app.get("/swag", async (req,res)=>{
+    const name = req.params.id;
+    try{
+        res.render('swag');
     }catch(error){
         res.status(500).send("Page not found");
     }
